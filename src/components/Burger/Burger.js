abe3a8d95bedc23888f1f5877ccac330e61eb86c
cchromak <cchromak@gmail.com>
2021-01-08 11:23:32 -0500

    hello

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,14 +3,21 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.css';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />
-            });
-        });
-        // transform an object of key/value pairs into an array of burger ing.
-        // where value of object => how many items, key => what kind of items.
+            });      // transform an object of key/value pairs into an array of burger ing.
+                     // where value of object => how many items, key => what kind of items.
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);  // flattens array
+    
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>;
+    }
+
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,7 +5,6 @@ import classes from './Layout.css';
 
 const layout = (props) => (
         <Auxiliary>
-            <div>Toolbar, Sidedrawer, Backdrop</div>
             <main className={classes.Content}>
                 {props.children}
             </main>
