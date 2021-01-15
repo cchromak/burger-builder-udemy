1294c19eb4cac06d2073960f82cf7887bbf09eef
cchromak <cchromak@gmail.com>
2021-01-15 14:29:02 -0500

    Added checkout summary component

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -6,16 +6,24 @@ import Auxiliary from '../Auxiliary';
 const withErrorHandler = ( WrappedComponent, axios ) => {
     return class extends Component {
         state = {
-            error: null
+            error: null,
+            reqInterceptor: null,
+            resInterceptor: null
         }
-        componentDidMount() {
-            axios.interceptors.request.use(req => {
+        componentWillMount() {
+            this.reqInterceptor = axios.interceptors.request.use(req => {
                 this.setState({error: null});
                 return req;
             })
-            axios.interceptors.response.use(res => res, error => {
+            this.resInterceptor =  axios.interceptors.response.use(res => res, error => {
                 this.setState({error: error})                
             });
+        }
+
+        componentWillUnmount() {
+            console.log('Will Unmount', this.resInterceptor, this.reqInterceptor);
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
         }
 
         errorConfirmedHandler = () => {
