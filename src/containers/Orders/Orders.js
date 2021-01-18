fec09e01b8112d937e02b0e188c17b75484aa4ca
cchromak <cchromak@gmail.com>
2021-01-18 13:10:02 -0500

    added fetching orders via axios, errorHandler hoc

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,8 +1,39 @@
 import React, { Component } from 'react';
 import Order from '../../components/Order/Order';
 
+import axios from '../../axios-oders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    // Fetch orders, component will only mount if there are new orders
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(res => {
+                const fetchOrders = [];
+                // Distribute properites of order object with spread op into array, adding one new properite order id (key)
+                for (let key in res.data) {
+                    fetchOrders.push({
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+                console.log(fetchOrders);
+                this.setState({
+                    loading: false
+                })
+            })
+            .catch(err => {
+                this.setState({
+                    loading: true
+                })
+            });
+    };
+
     render() {
         return(
             <div>
@@ -14,4 +45,4 @@ class Orders extends Component {
     }
 };
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
